80d36090d777424b08f7e756bf6ec99053206308
githubscientist <sathish.datascientist@gmail.com>
2023-11-25 11:46:00 +0530

    time doubt

--- session1/homework.js
+++ session1/homework.js
@@ -4,3 +4,24 @@
         https://leetcode.com/problems/two-sum/
 
 */
+
+/*
+    time complexity:
+
+    capture the time initially <- t1
+
+    do something / run the code / call the function
+
+    capture the time after the code is executed <- t2
+
+    time taken = t2 - t1
+
+    ms / s / microseconds / nanoseconds
+
+    it depends on several factors:
+        - the size of the input
+        - the complexity of the code
+        - the hardware (CPU, RAM, Virtual Memory, Hard Disk or SSD, etc.)
+
+    there is no constant unit of measurement for time complexity -> asymptotic notations
+*/
